6e163614e4424f643b97cbbea8e9dfcac04467ad
Lucas Ivisson <lucasivisson1@gmail.com>
2023-11-03 12:27:43 -0300

    ✨ introduce food and creating random number functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 const canvas = document.querySelector('canvas')
+const image = document.getElementById("source");
 const ctx = canvas.getContext('2d')
 
-const squareSize = 20
+const squareSize = 30
 
-const snake = [{x: 200, y:200}, {x: 180, y:200}]
+const snake = [{x: 210, y:210}, {x: 180, y:210}]
+
+const randomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+const number = randomPosition(0, canvas.width - squareSize)
+}
+
+const food = {
+  x: randomPosition(),
+  y: 90,
+}
 
 let direction, loopId
+
+const drawFood = () => {
+  const { x, y } = food
+  ctx.shadowColor = "white"
+  ctx.shadowBlur = 8
+  ctx.drawImage(image, x, y, squareSize, squareSize)
+  ctx.shadowBlur = 0
+}
 
 const drawSnake = () => {
   snake.forEach((position, index) => {
@@ -44,28 +66,37 @@ const moveSnake = () => {
 
 const drawGrid = () => {
   ctx.lineWidth = 1
-  ctx.strokeStyle = "red"
+  ctx.strokeStyle = "#191919"
 
-  ctx.lineTo(500, 0)
-  ctx.lineTo(500, 500)
-  ctx.stroke()
+  for (let i=squareSize; i<canvas.width; i+=squareSize) {
+      ctx.beginPath()
+      ctx.lineTo(i, 0)
+      ctx.lineTo(i, 510)
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.lineTo(0, i)
+      ctx.lineTo(900, i)
+      ctx.stroke()
+  }
+  
 }
-
-drawGrid()
 
 const gameLoop = () => {
   clearInterval(loopId)
-  ctx.clearRect(0, 0, 1000, 500)
+  ctx.clearRect(0, 0, 900, 510)
 
+  drawGrid()
+  drawFood()
   moveSnake()
   drawSnake()
 
   loopId = setInterval(() => {
     gameLoop()
-  }, 100)
+  }, 200)
 }
 
-// gameLoop()
+gameLoop()
 
 document.addEventListener("keydown", ({ key }) => {
   if(key == "ArrowRight" && direction !== "left") {
