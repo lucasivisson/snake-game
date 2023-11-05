3972801fde440fa47b374ce3057b84c23ab24698
Lucas Ivisson <lucasivisson1@gmail.com>
2023-11-05 14:52:33 -0300

    .

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = http.createServer(app);
+// const io = new Server(server);
+
+app.listen(3000);
+app.use(express.static("public"));
